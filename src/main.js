09b99daac5c327d6e447b61857ae3f597caa1d7d
Deven Phillips <deven.phillips@redhat.com>
2018-09-28 11:36:27 -0400

    Initial reworking of Quasar demo code into rough draft

--- src/main.js
+++ src/main.js
@@ -3,6 +3,54 @@ import App from './App.vue';
 import router from './router';
 import './registerServiceWorker';
 
+import './styles/quasar.styl';
+import 'quasar-framework/dist/quasar.ie.polyfills';
+import iconSet from 'quasar-framework/icons/fontawesome';
+import 'quasar-extras/material-icons';
+import 'quasar-extras/fontawesome';
+import {
+  Quasar,
+  QBtn,
+  QLayout,
+  QLayoutHeader,
+  QLayoutDrawer,
+  QPage,
+  QPageContainer,
+  QToolbar,
+  QToolbarTitle,
+  QList,
+  QListHeader,
+  QItemSeparator,
+  QItem,
+  QItemSide,
+  QItemMain,
+} from 'quasar';
+
+Vue.use(Quasar, {
+  config: {},
+  components: {
+    QBtn,
+    QLayout,
+    QLayoutHeader,
+    QLayoutDrawer,
+    QPage,
+    QPageContainer,
+    QToolbar,
+    QToolbarTitle,
+    QList,
+    QListHeader,
+    QItemSeparator,
+    QItem,
+    QItemSide,
+    QItemMain,
+  },
+  directives: {
+  },
+  plugins: {
+  },
+  iconSet,
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
---
+++ src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="row">
+      <img alt="Homeland Security Logo" src="../assets/logo.png" class="logo">
+    </div>
+  </q-page>
+</template>
+
+<style lang="less" scoped="true">
+logo {
+  width: 60px;
+  height: 60px;
+}
+</style>
+
+<script>
+export default {
+  name: 'Login',
+};
+</script>
